3a540c2774f27c077e07b663f3f8223d0e425597
Russell Ruffolo <ruffolorussell@gmail.com>
2024-01-04 18:19:07 -0700

    Fixed degree/minute/second handling

--- createDB.js
+++ createDB.js
@@ -9,7 +9,7 @@ fs.createReadStream("./PA22.csv")
     db.serialize(function () {
       db.run(
         `INSERT INTO bridges VALUES (?, ? , ?, ?)`,
-        [row[1], insertDecimal(row[19]), -insertDecimal(row[20]), "Road: " + row[12] + "\nCrosses: " + row[10] + "\nLocation: " + row[13]],
+        [row[1], parseLatitude(row[19]), -parseLongitude(row[20]), "Road: " + row[12] + "\nCrosses: " + row[10] + "\nLocation: " + row[13]],
         function (error) {
           if (error) {
             return console.log(error.message);
@@ -18,6 +18,44 @@ fs.createReadStream("./PA22.csv")
       );
     });
   });
+
+  function parseLatitude(latValue){
+    var latString = String(latValue);
+
+    var degrees = latString.slice(0,2);
+    var minutes = latString.slice(2,4);
+    var secondsBeforeDecimal = latString.slice(4,6);
+    var secondsAfterDecimal = latString.slice(6);
+
+    var seconds = secondsBeforeDecimal + "." + secondsAfterDecimal;
+
+    var degFloat = parseFloat(degrees);
+    var minuteFloat = parseFloat(minutes) / 60;
+    var secondsFloat = parseFloat(seconds) / 3600;
+
+    var latitudeDecimal = degFloat + minuteFloat + secondsFloat;
+    return latitudeDecimal;
+
+
+  }
+
+  function parseLongitude(longValue){
+    var longString = String(longValue);
+
+    var degrees = longString.slice(1,3);
+    var minutes = longString.slice(3,5);
+    var secondsBeforeDecimal = longString.slice(5,7);
+    var secondsAfterDecimal = longString.slice(7);
+
+    var seconds = secondsBeforeDecimal + "." + secondsAfterDecimal;
+
+    var degFloat = parseFloat(degrees);
+    var minuteFloat = parseFloat(minutes) / 60;
+    var secondsFloat = parseFloat(seconds) / 3600;
+
+    var longitudeDecimal = degFloat + minuteFloat + secondsFloat;
+    return longitudeDecimal;
+  }
 
   function insertDecimal(num) {
     return Number((num / 1000000));
